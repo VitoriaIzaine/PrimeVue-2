beeee3c272d19ea9a64f594b97c2d301a16e49d9
vitoriaIzaine <vitoriaizaine@gmail.com>
2021-10-25 11:23:10 -0300

    Aula de PrimeVue

--- src/main.js
+++ src/main.js
@@ -3,4 +3,26 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import Primevue from 'primevue/config'
+
+import 'primevue/resources/themes/saga-blue/theme.css'      
+import 'primevue/resources/primevue.min.css'                
+import 'primeicons/primeicons.css'
+
+// PrimeVUe Componentes
+import Knob from 'primevue/knob';
+import Password from 'primevue/password';
+import InputText from 'primevue/inputtext';
+
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.use(Primevue);
+
+app.component('Knob',Knob);
+app.component('Password',Password);
+app.component('InputText',InputText);
+
+
+
+app.mount('#app');
--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,17 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user:{
+      name: 'Vitoria',
+      email:'vitoriaizaine@gmail.com',
+    }
   },
   mutations: {
+    
+    registerUser(state, payLoad){
+      state.user.name = payLoad;
+      console.log("Nome aterado!!!!!!!!!!!!!!!!");
+    }
   },
   actions: {
   },
